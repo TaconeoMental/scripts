dbff4211290235fff9d60e99d8c1aeea9fe5fc79
Mateo <25857822+TaconeoMental@users.noreply.github.com>
2024-10-28 00:33:48 -0300

    SMALL changes...

--- helpers/pve-rename.sh
+++ helpers/pve-rename.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# Ugly code, but it works
+# Ugly code. Made in a few hours. Tested. It works.
 
 CURRENT_HOSTNAME=$(hostname)
 
 function usage() {
     trap - EXIT
-    echo "Usage: $(basename $0) -H|--hostname [new hostname]"
+    cat << EOF
+Usage: $(basename $0) ARGS
+
+ARGS:
+    --new-hostname [new hostname]
+    --old-hostname [old hostname]
+EOF
     exit 1
 }
 
@@ -19,34 +25,63 @@ function confirm_or_die() {
     fi
 }
 
+while test $# != 0
+do
+    case "$1" in
+    --new-hostname)
+        NEW_HOSTNAME="$2"; shift ;;
+    --old-hostname)
+        OLD_HOSTNAME="$2"; shift ;;
+    --) shift; break;;
+    *)  usage;;
+    esac
+    shift
+done
+
+if [ -z "$NEW_HOSTNAME" ]
+then
+    usage
+fi
+
 function pre_reboot() {
-    local new_hostname="$1"
+	if [ "$OLD_HOSTNAME" != "$CURRENT_HOSTNAME" ]; then
+	  echo "[-] Old hostname doesn't match with $CURRENT_HOSTNAME"
+      exit 1
+	fi
+
+    confirm_or_die "The hostname of this node will change from '$CURRENT_HOSTNAME' to '$NEW_HOSTNAME'"
+
     # Update hosts file
     echo "[+] Updating /etc/hosts file"
-    sed -i -e "s/$CURRENT_HOSTNAME/$new_hostname/g" /etc/hosts
+    sed --in-place --expression "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 
     # Update hostname file
     echo "[+] Updating /etc/hostname file"
-    sed -i -e "s/$CURRENT_HOSTNAME/$new_hostname/g" /etc/hostname
+    sed --in-place --expression "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
 
     echo "[+] Modifying rrdcached directories"
-    cp /var/lib/rrdcached/db/pve2-node/$CURRENT_HOSTNAME /var/lib/rrdcached/db/pve2-node/$new_hostname
-    cp -r /var/lib/rrdcached/db/pve2-storage/$CURRENT_HOSTNAME /var/lib/rrdcached/db/pve2-storage/$new_hostname
-    echo "[+] Deleting old rrdcached directories"
-    rm /var/lib/rrdcached/db/pve2-node/$CURRENT_HOSTNAME
-    rm -r /var/lib/rrdcached/db/pve2-storage/$CURRENT_HOSTNAME
+    cp "/var/lib/rrdcached/db/pve2-node/$CURRENT_HOSTNAME" "/var/lib/rrdcached/db/pve2-node/$NEW_HOSTNAME"
+    cp --recursive "/var/lib/rrdcached/db/pve2-storage/$CURRENT_HOSTNAME" "/var/lib/rrdcached/db/pve2-storage/$NEW_HOSTNAME"
 }
 
 function post_reboot() {
-    echo "post reboot"
+    echo "[*] Running post reboot..."
+
     local nodes_dir="/etc/pve/nodes"
     local old_node_dir=$nodes_dir/$(ls --ignore "$NEW_HOSTNAME" $nodes_dir)
     echo "[*] Moving $old_node_dir/lxc/* to $nodes_dir/$NEW_HOSTNAME/lxc/"
-    mv  $old_node_dir/lxc/* "$nodes_dir/$NEW_HOSTNAME/lxc/"
+    cp --no-clobber --recursive $old_node_dir/lxc/* "$nodes_dir/$NEW_HOSTNAME/lxc/"
     echo "[*] Moving $old_node_dir/qemu-server/* to $nodes_dir/$NEW_HOSTNAME/qemu-server/"
-    mv $old_node_dir/qemu-server/* "$nodes_dir/$NEW_HOSTNAME/qemu-server/"
-    echo "[*] Removing old node"
+    cp --no-clobber --recursive $old_node_dir/qemu-server/* "$nodes_dir/$NEW_HOSTNAME/qemu-server/"
+    #echo "[*] Removing old node"
     #rm -r $old_node_dir
+}
+
+function cleanup() {
+    echo "[*] Cleaning up..."
+    return
+    rm --verbose "/var/lib/rrdcached/db/pve2-node/$OLD_HOSTNAME"
+    rm --verbose --recursive "/var/lib/rrdcached/db/pve2-storage/$OLD_HOSTNAME"
 }
 
 function is_first_time() {
@@ -62,46 +97,16 @@ function is_first_time() {
     return 0
 }
 
-function is_post_reboot() {
-    if [ `ls -1 /etc/pve/nodes/$NEW_HOSTNAME 2>/dev/null | wc -l` -eq 0 ]
-    then
-        return 1
-    fi
-    return 0
-}
-
-while test $# != 0
-do
-    case "$1" in
-    -H|--hostname)
-        NEW_HOSTNAME="$2"; shift ;;
-    --) shift; break;;
-    *)  usage;;
-    esac
-    shift
-done
-
-if [ -z "$NEW_HOSTNAME" ]
-then
-    usage
-fi
-
-
 is_first_time
 first_time=$?
 if [[ $first_time -eq 1 ]]
 then
-    confirm_or_die "The hostname of this node will change from '$CURRENT_HOSTNAME' to '$NEW_HOSTNAME'"
-    pre_reboot $NEW_HOSTNAME
+    pre_reboot
     confirm_or_die "The host must reboot for the changes to take effect. It must be run again after the reboot"
     reboot now
 fi
 
-is_post_reboot
-post_reboot=$?
-if [[ $post_reboot -eq 0 ]]
-then
-    post_reboot 
-fi
+post_reboot
+cleanup
 
 echo "[+] All done"
